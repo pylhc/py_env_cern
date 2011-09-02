d1319cbba2a4dd30637cf7b2df816cad151cf2fa
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-09-02 18:56:38 +0200

    added stuff

---
+++ gcc45-setup.sh
@@ -0,0 +1,23 @@
+gcc_config_version=4.5.2
+mpfr_config_version=2.3.1
+gmp_config_version=4.2.2
+LCGPLAT=x86_64-slc5
+LCG_lib_name=lib64
+
+if [ -z $1 ] 
+then LCG_contdir=/afs/cern.ch/sw/lcg/contrib
+else LCG_contdir=$1 
+fi
+LCG_gcc_home=${LCG_contdir}/gcc/${gcc_config_version}/${LCGPLAT}
+LCG_mpfr_home=${LCG_contdir}/mpfr/${mpfr_config_version}/${LCGPLAT}
+LCG_gmp_home=${LCG_contdir}/gmp/${gmp_config_version}/${LCGPLAT}
+
+export PATH=${LCG_gcc_home}/bin:${PATH}
+
+if [ ${LD_LIBRARY_PATH} ] 
+then
+export LD_LIBRARY_PATH=${LCG_gcc_home}/${LCG_lib_name}:${LCG_mpfr_home}/lib:${LCG_gmp_home}/lib:${LD_LIBRARY_PATH}
+else
+export LD_LIBRARY_PATH=${LCG_gcc_home}/${LCG_lib_name}:${LCG_mpfr_home}/lib:${LCG_gmp_home}/lib
+fi
+
